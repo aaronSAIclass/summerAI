afb7693484cac9ca7d56682a18fd4be56e9294ee
denobody2 <amschildkraut@gmail.com>
2023-07-13 15:47:53 -0400

    a

--- staff.py
+++ staff.py
@@ -22,10 +22,5 @@ class Staff:
     def moveZ(self, z1):
         self.zPos += z1
 
-s1 = Staff("Math Teacher", 1, 3, 1, True, 0)
-s2 = Staff("English Teacher", 0, 2, 4, False, 1)
-s3 = Staff("History Teacher", 3, 3, 1, False, 2)
-s4 = Staff("Biology Teacher", 4, 3, 4, False, 4)
-s5 = Staff("Janitor", 1, 2, 2, False, 5)
-s6 = Staff("Headmaster", 0, 0, 1, False, 7)
-s7 = Staff("Art Teacher", 1, 2, 4, False, 6)
+
+
